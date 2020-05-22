104ee0bf45b519b5e2aaa607d5ed6b772e1c4926
ashtonmorgan <ashtomorgan3@gmail.com>
2020-05-22 11:05:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ashton_morgan_student_scheduler_c196/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ashton_morgan_student_scheduler_c196;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

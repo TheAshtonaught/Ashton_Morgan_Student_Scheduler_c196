4d817a3a918facfaf293b9e641d49aca46e59ea2
ashtonmorgan <ashtomorgan3@gmail.com>
2020-06-02 00:06:57 -0500

    Created add new term screen

---
+++ app/src/main/java/com/example/ashton_morgan_student_scheduler_c196/AddTermActivity.java
@@ -0,0 +1,121 @@
+package com.example.ashton_morgan_student_scheduler_c196;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AddTermActivity extends AppCompatActivity {
+    public static final String EXTRA_TERM_TITLE =
+            "com.example.ashton_morgan_student_scheduler_c196.EXTRA_TERM_TITLE";
+    public static final String EXTRA_START_DATE_STRING =
+            "com.example.ashton_morgan_student_scheduler_c196.EXTRA_START_DATE_STRING";
+    public static final String EXTRA_END_DATE_STRING =
+            "com.example.ashton_morgan_student_scheduler_c196.EXTRA_END_DATE_STRING";
+
+    private EditText editTextTitle;
+    private EditText editTextStartDate;
+    private EditText editTextEndDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_term);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextStartDate = findViewById(R.id.edit_text_start_date);
+        editTextEndDate = findViewById(R.id.edit_text_end_date);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Term");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_term_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_term:
+                //save term
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveTerm() {
+        String title = editTextTitle.getText().toString();
+        String startDateString = editTextStartDate.getText().toString();
+        String endDateString = editTextEndDate.getText().toString();
+
+        if (title.trim().isEmpty()) {
+            Toast.makeText(this, "Please enter a title", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            Date startDate = new SimpleDateFormat("MM/dd/yyyy").parse(startDateString);
+            Date endDate = new SimpleDateFormat("MM/dd/yyyy").parse(endDateString);
+
+            Intent data = new Intent();
+            data.putExtra(EXTRA_TERM_TITLE, title);
+            data.putExtra(EXTRA_START_DATE_STRING, startDateString);
+            data.putExtra(EXTRA_END_DATE_STRING, endDateString);
+
+            setResult(RESULT_OK, data);
+            finish();
+
+        } catch (ParseException e) {
+            Toast.makeText(this, "Enter a valid date format MM/dd/yyyy", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return;
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
